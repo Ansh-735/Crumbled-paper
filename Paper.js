cb04cb35cf32128800efd00a4536bc085de13cc6
Ansh-735 <72873662+Ansh-735@users.noreply.github.com>
2020-11-18 17:11:55 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,26 @@
+class Paper{
+  constructor(x,y,r){
+  var options={
+    isStatic:false ,
+    restitution:0.3, 
+    friction:0.5,
+     density:1.2
+    }
+  this.x=x
+  this.y=y
+  this.r=20
+  this.body= Bodies.circle(this.x,this.y,this.r/2,options)
+  World.add(world,this.body)
+  }
+  display(){
+  var paperPos=(this.body.position) 
+  push()
+  translate(paperPos.x,paperPos.y);
+  ellipseMode(RADIUS);
+  fill("blue");
+  
+  
+  ellipse(0,0,this.r,this.r);
+  pop()
+  }
+   }
